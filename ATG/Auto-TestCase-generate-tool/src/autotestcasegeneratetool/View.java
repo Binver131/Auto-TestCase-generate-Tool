a79058ac3172bcfd8630daa73bcc8ebbbb5db4b0
Binver131 <59934346+Binver131@users.noreply.github.com>
2020-04-10 02:18:00 +0800

    save

--- ATG/Auto-TestCase-generate-tool/src/autotestcasegeneratetool/View.java
+++ ATG/Auto-TestCase-generate-tool/src/autotestcasegeneratetool/View.java
@@ -35,8 +35,6 @@ public class View extends ViewPart {
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		this.setTitle(requirementID);
-		
 		Composite top = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
 		layout.marginHeight = 0;
