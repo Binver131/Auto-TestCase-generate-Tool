7bf6865926a5277209c6e9dca7431ac7a9453d95
Binver131 <59934346+Binver131@users.noreply.github.com>
2020-05-20 13:56:50 +0800

    Revert "Merge branch 'commit' of https://github.com/Binver131/Auto-TestCase-generate-Tool into commit"
    This reverts commit ba061175cc35daece3d6f09ee9d9e1a5653a04b7, reversing
    changes made to 0301f3e487c9f8e053c68030008a6e2a2beb972d.

--- ATG/Auto-TestCase-generate-tool/src/entity/Type.java
+++ ATG/Auto-TestCase-generate-tool/src/entity/Type.java
@@ -8,29 +8,13 @@ package entity;
 * @date 2020年5月14日
  */
 public class Type {
-	private int typeID;			//类型ID号
+	private String typeID;			//类型ID号
 	private String typename;	//类型名称
 	private String typerange;	//类型名
-	private int modelid;
+	private String modelID;
 	private String sizeString;
-	public int getModelid() {
-		return modelid;
-	}
-	public void setModelid(int modelid) {
-		this.modelid = modelid;
-	}
-	public String getSizeString() {
-		return sizeString;
-	}
-	public void setSizeString(String sizeString) {
-		this.sizeString = sizeString;
-	}
-	public int getTypeID() {
-		return typeID;
-	}
-	public void setTypeID(int typeID) {
-		this.typeID = typeID;
-	}
+	
+	
 	public String getTypename() {
 		return typename;
 	}
@@ -45,6 +29,37 @@ public class Type {
 	}
 	@Override
 	public String toString() {
-		return "Type [typeID=" + typeID + ", typename=" + typename + ", typerange=" + typerange + "]";
+		return "Type [typeID="  + ", typename=" + typename + ", typerange=" + typerange + "]";
+	}
+	
+	public String getSizeString() {
+		return sizeString;
+	}
+	public void setSizeString(String sizeString) {
+		this.sizeString = sizeString;
+	}
+	/**
+	 * @return the typeID
+	 */
+	public String getTypeID() {
+		return typeID;
+	}
+	/**
+	 * @param typeID the typeID to set
+	 */
+	public void setTypeID(String typeID) {
+		this.typeID = typeID;
+	}
+	/**
+	 * @return the modelID
+	 */
+	public String getModelID() {
+		return modelID;
+	}
+	/**
+	 * @param modelID the modelID to set
+	 */
+	public void setModelID(String modelID) {
+		this.modelID = modelID;
 	}
 }
