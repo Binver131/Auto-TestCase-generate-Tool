61411c0ea1fa82533a943cf33df60f4285d4f5d5
Binver131 <59934346+Binver131@users.noreply.github.com>
2020-05-26 22:22:55 +0800

    修复表格宽度自动调整功能

--- ATG/Auto-TestCase-generate-tool/src/handlers/DeleteModelHandler.java
+++ ATG/Auto-TestCase-generate-tool/src/handlers/DeleteModelHandler.java
@@ -1,28 +1,48 @@
 package handlers;
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 import console.ConsoleHandler;
-import jdbc.ConnectHelper;
+import entity.Model;
 import views.NavigationView;
 
 
 public class DeleteModelHandler extends AbstractHandler {
 
+	
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ConsoleHandler.info("刪除模型");
-		//TODO 從數據模型 database裡面移除此模型  ，從數據庫中刪除此模型 及測試用例
 		
-		ConnectHelper.removeVariable("4");
-		ConnectHelper.remodeType("4");
-		ConnectHelper.removeRequirement("4");
-		ConnectHelper.removeRowRequirement("4");
-		ConnectHelper.removeModel("4");
+		//TODO 從數據模型 database裡面移除此模型  ，從數據庫中刪除此模型 及測試用例
+		ISelection selection =  PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
+		if(selection!= null && selection instanceof IStructuredSelection){
+			IStructuredSelection is = (IStructuredSelection)selection;
+			
+			
+			if(is.getFirstElement() instanceof Model) {
+				Iterator<Model> it = is.iterator();
+				while (it.hasNext()) {
+					Model model = (Model) it.next();
+					ConsoleHandler.info("The Model "+model.toString()+" will be deleted");
+				}
+			}
+		}
+//		ConnectHelper.removeVariable("4");
+//		ConnectHelper.remodeType("4");
+//		ConnectHelper.removeRequirement("4");
+//		ConnectHelper.removeRowRequirement("4");
+		
+        
+		//ConnectHelper.removeModel();
 		
 		//刷新界面
 		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
